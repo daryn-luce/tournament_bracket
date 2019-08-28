0978ae8ee1a4cb20af0ececf4225c9ef29e1a692
daryn.luce <daryn.luce@gmail.com>
2019-08-28 17:27:11 -0500

    update

--- t_bracket.py
+++ t_bracket.py
@@ -28,12 +28,37 @@ def add_player(bracket,player_list):
             if len(temp_list) < 1:
                 break
 
+def player_win(bracket,player):
+    winner = False
+    for b in bracket:
+        if b[1][0] == player and b[2][0] != '':
+            b[1][1] = 1
+            b[2][1] = 0
+            loser_name = b[2][0]
+        elif b[2][0] == player and b[1][0] != '':
+            b[1][1] = 0
+            b[2][1] = 1
+            loser_name = b[1][0]
+        if b[0][:-1] == 'Game':
+            for i in range(len(b) -1,1,-1):
+                if b[i][0] == '':
+                    b[i][0] = player
+                    winner = True
+                    break
+        if b[0][:-1] == 'Loser':
+            for i in range(len(b) -1,1,-1):
+                if b[i][0] == '':
+                    b[i][0] = loser_name
+                    break
+        if winner:
+            break
 
 player_list = ['Keegan','Mitch','William','Daryn','Felisha']
 
 bracket = create_bracket(len(player_list))
 
 add_player(bracket,player_list)
+player_win(bracket,player_list[3])
 
 for b in bracket:
     print(b)
